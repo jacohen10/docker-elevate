8ccd6f4efcfd77204accb72718d312fcbd15ca94
Josh Cohen <jacohen10@EMAIL.COM>
2015-12-14 19:47:11 -0500

    improve customer-authentication

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,5 +1,5 @@
 class CustomersController < ApplicationController
-  before_action :set_customer, only: [:show,:edit,:update,:destroy]
+  before_action :set_customer, only: [:show,:edit,:update]
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   load_and_authorize_resource
 
@@ -16,16 +16,22 @@ class CustomersController < ApplicationController
   end
 
   def new
-    @customer = Customer.new
+    if current_user.customers.count < 1
+      @customer = Customer.new
+    else
+      redirect_to '/'
+    end
   end
 
   def create
     @customer = current_user.customers.create(customer_params)
-
     if current_user.role == "admin"
       redirect_to customers_path
+
     elsif current_user.role == "customer"
       redirect_to user_customer_path(current_user, @customer)
+    else
+      redirect_to '/'
     end
   end
 
@@ -33,13 +39,19 @@ class CustomersController < ApplicationController
   end
 
   def update
-    @customer.update(customer_params)
-    redirect_to customer_path(@customer)
+      @customer.update(customer_params)
+      redirect_to customer_path(@customer)
   end
 
   def destroy
-    @customer.destroy
-    redirect_to customers_path
+    if current_user.role == "admin"
+      @customer = Customer.find(params[:id])
+      @customer.destroy
+      redirect_to customers_path
+    else
+      redirect_to '/'
+    end
+
   end
 
 
@@ -50,7 +62,11 @@ class CustomersController < ApplicationController
   end
 
   def set_customer
-    @customer = Customer.find(params[:id])
+    if current_user.role == "admin"
+      @customer = Customer.find(params[:id])
+    elsif current_user.role == "customer"
+      @customer = current_user.customers.find(params[:id])
+    end
   end
 
 end
